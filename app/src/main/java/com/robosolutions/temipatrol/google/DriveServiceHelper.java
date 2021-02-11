bf9e06705d7c622b68d3a561492e8ec7cab1f43b
chanjunren <chanjunren@gmail.com>
2021-02-11 09:15:12 +0800

    Implemented code for uploading a file to Google Drive

--- app/src/main/java/com/robosolutions/temipatrol/google/DriveServiceHelper.java
+++ app/src/main/java/com/robosolutions/temipatrol/google/DriveServiceHelper.java
@@ -2,8 +2,14 @@ package com.robosolutions.temipatrol.google;
 
 import android.content.Context;
 
+import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
 
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -17,148 +23,27 @@ public class DriveServiceHelper {
         this.mDriveService = mDriveService;
     }
 
-//    public DriveServiceHelper getInstanceFor(Context context) {
-//        if (INSTANCE == null) {
-//            Log.e(TAG, "DriveServiceHelper has not been initialized");
-//            Toast.makeText(context, "Error: DriveServiceHelper has not been initialized",
-//                    Toast.LENGTH_LONG).show();
-//        }
-//        INSTANCE.setContext(context);
-//        return INSTANCE;
-//
-//    }
-//
-//    /**
-//     * Returns an {@link Intent} for opening the Storage Access Framework file picker.
-//     */
-//    public Intent createImgPickerIntent() {
-//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//        intent.addCategory(Intent.CATEGORY_OPENABLE);
-//        intent.setType("image/*");
-//        return intent;
-//    }
-//
-//
-//    /**
-//     * Opens the file at the {@code uri} returned by a Storage Access Framework {@link Intent}
-//     * created by {@link #createImgPickerIntent()} using the given {@code contentResolver}.
-//     */
-//    public Task<ImageAction> downloadImgUsingStorageAccessFramework(
-//            ContentResolver contentResolver, Uri uri) {
-//        return Tasks.call(mExecutor, () -> {
-//            // Retrieve document's display name from metadata
-//            try {
-//                String name;
-//                Cursor cursor = contentResolver.query(uri, null, null, null, null);
-//                if (cursor != null && cursor.moveToFirst()) {
-//                    int nameIdx = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-//                    name = cursor.getString(nameIdx);
-//                } else {
-//                    throw new IOException("Empty cursor returned for file");
-//                }
-//                InputStream is = contentResolver.openInputStream(uri);
-//                String outputPath = context.getExternalFilesDir(Values.IMAGE_DIR).getPath() + "/" + name;
-//                Log.i(TAG, "current path: " + outputPath);
-//                File newImgFile = new File(outputPath);
-//                if (!newImgFile.exists()) {
-//                    newImgFile.getParentFile().mkdirs();
-//                    newImgFile.createNewFile();
-//                }
-//
-//                byte[] inputData = getBytes(is);
-//                writeFile(inputData, outputPath);
-//
-//                int THUMB_SIZE = 64;
-//                Bitmap bitmap = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(newImgFile.getAbsolutePath()),
-//                        THUMB_SIZE, THUMB_SIZE);
-//                ImageAction action = new ImageAction(outputPath, bitmap, -1, name);
-//
-//                return action;
-//
-//            }catch (Exception e) {
-//                Log.e(TAG, e.toString());
-//                return null;
-//            }
-//        });
-//    }
-//
-//    /**
-//     * Returns an {@link Intent} for opening the Storage Access Framework file picker.
-//     */
-//    public Intent createVidPickerIntent() {
-//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//        intent.addCategory(Intent.CATEGORY_OPENABLE);
-//        intent.setType("video/*");
-//        return intent;
-//    }
-//
-//
-//    /**
-//     * Opens the file at the {@code uri} returned by a Storage Access Framework {@link Intent}
-//     * created by {@link #createImgPickerIntent()} using the given {@code contentResolver}.
-//     */
-//    public Task<Pair<String, String>> downloadVidUsingStorageAccessFramework(
-//            ContentResolver contentResolver, Uri uri) {
-//        return Tasks.call(mExecutor, () -> {
-//            // Retrieve document's display name from metadata
-//            try {
-//                String name;
-//                Cursor cursor = contentResolver.query(uri, null, null, null, null);
-//                if (cursor != null && cursor.moveToFirst()) {
-//                    int nameIdx = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-//                    name = cursor.getString(nameIdx);
-//                } else {
-//                    throw new IOException("Empty cursor returned for file");
-//                }
-//                InputStream is = contentResolver.openInputStream(uri);
-//                String outputPath = context.getExternalFilesDir(Values.VIDEO_DIR).getPath() + "/" + name;
-//                Log.i(TAG, "current path: " + outputPath);
-//                File newVidFile = new File(outputPath);
-//                if (!newVidFile.exists()) {
-//                    newVidFile.getParentFile().mkdirs();
-//                    newVidFile.createNewFile();
-//                }
-//
-//                byte[] inputData = getBytes(is);
-//                writeFile(inputData, outputPath);
-//
-//                return Pair.create(name, outputPath);
-//
-//            }catch (Exception e) {
-//                Log.e(TAG, e.toString());
-//                return null;
-//            }
-//        });
-//    }
-//
-//    /** This function puts everything in the provided InputStream into a byte array
-//     * and returns it to the calling function. */
-//    public byte[] getBytes(InputStream inputStream) throws IOException {
-//
-//        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-//        int bufferSize = 1024;
-//        byte[] buffer = new byte[bufferSize];
-//
-//        int len = 0;
-//
-//        while ((len = inputStream.read(buffer)) != -1) {
-//            byteBuffer.write(buffer, 0, len);
-//        }
-//
-//        return byteBuffer.toByteArray();
-//    }
-//
-//    /**   This function rewrites the byte array to the provided filename.
-//     *
-//     * Note: A String, NOT a file object, though you could easily tweak it to do
-//     * that. */
-//    public void writeFile(byte[] data, String fileName) throws IOException{
-//        FileOutputStream out = new FileOutputStream(fileName);
-//        out.write(data);
-//        out.close();
-//    }
-//
-//    public void setContext(Context context) {
-//        this.context = context;
-//    }
+    public void uploadFile(java.io.File file, String folderId) throws IOException {
+        File fileMetadata = new File();
+        fileMetadata.setName(file.getName());
+        FileContent mediaContent = new FileContent("image/jpeg", file);
+        fileMetadata.setParents(Collections.singletonList(folderId));
+        File googleFile = mDriveService.files().create(fileMetadata, mediaContent)
+                .setFields("id, parents")
+                .execute();
+        System.out.println("File ID: " + googleFile.getId());
+    }
+
+    // Google folder mime type: application/vnd.google-apps.folder
+    // Creates folder and returns the folder ID
+    public String createFolder(String date) throws IOException {
+        File fileMetadata = new File();
+        fileMetadata.setName(date);
+        fileMetadata.setMimeType("application/vnd.google-apps.folder");
+
+        File file = mDriveService.files().create(fileMetadata)
+                .setFields("id")
+                .execute();
+        return file.getId();
+    }
 }
