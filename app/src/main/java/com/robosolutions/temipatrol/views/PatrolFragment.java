c4a0dfee21ed77be793b2244c01d7be0baf63484
Jun Ren <chanjunren@gmail.com>
2021-02-19 11:40:28 +0800

    Implemented cameraX preview

--- app/src/main/java/com/robosolutions/temipatrol/views/PatrolFragment.java
+++ app/src/main/java/com/robosolutions/temipatrol/views/PatrolFragment.java
@@ -1,53 +1,46 @@
 package com.robosolutions.temipatrol.views;
 
-import android.Manifest;
-import android.hardware.Camera;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.PermissionChecker;
+
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import com.robosolutions.temipatrol.R;
-import com.robosolutions.temipatrol.camera.CameraPreview;
 import com.robosolutions.temipatrol.client.JsonPostman;
-import com.robosolutions.temipatrol.client.JsonRequestUtils;
 import com.robosolutions.temipatrol.viewmodel.GlobalViewModel;
 
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import static androidx.core.content.PermissionChecker.checkSelfPermission;
-import static com.robosolutions.temipatrol.camera.CameraUtils.MEDIA_TYPE_IMAGE;
-import static com.robosolutions.temipatrol.camera.CameraUtils.getOutputMediaFile;
+import java.util.concurrent.ExecutionException;
 
 // Page shown when Temi is patrolling
 public class PatrolFragment extends Fragment {
     private static final String TAG = "PatrolFragment";
-    private Camera mCamera;
-    private CameraPreview mPreview;
     private GlobalViewModel viewModel;
+    private PreviewView previewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCamera = getCameraInstance();
-        mCamera.setDisplayOrientation(180);
-        mPreview = new CameraPreview(getContext(), mCamera);
         viewModel = new ViewModelProvider(getActivity()).get(GlobalViewModel.class);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
     }
 
     @Override
@@ -57,87 +50,30 @@ public class PatrolFragment extends Fragment {
         return inflater.inflate(R.layout.patrol_fragment, container, false);
     }
 
-    @Override
+        @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        FrameLayout previewLayout = view.findViewById(R.id.camera_preview);
-        previewLayout.addView(mPreview);
-
-        // Just for testing
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                mCamera.takePicture(null, null, mPictureCallback);
-                Log.i(TAG, "Camera released");
+        previewView = view.findViewById(R.id.previewView);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e(TAG, "CameraProvider initialization error: " + e.toString());
             }
-        }, 10000);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                releaseCamera();
-                Log.i(TAG, "Camera released");
-            }
-        }, 20000);
+        }, ContextCompat.getMainExecutor(getContext()));
     }
 
-    public Camera getCameraInstance() {
-        int cameraCount = 0;
-        Camera cam = null;
-        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
-        cameraCount = Camera.getNumberOfCameras();
-        if (checkSelfPermission(this.getContext() ,Manifest.permission.CAMERA)
-                != PermissionChecker.PERMISSION_GRANTED) {
-            Log.i(TAG, "Permission not granted");
-        }
-        for (int camIdx = 0; camIdx < cameraCount; camIdx++) {
-            Camera.getCameraInfo(camIdx, cameraInfo);
-            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-                try {
-                    cam = Camera.open(camIdx);
-                } catch (RuntimeException e) {
-                    Log.e(TAG, "Camera failed to open: " + e.getLocalizedMessage());
-                }
-            }
-        }
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build();
 
-        return cam;
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
     }
-
-    private Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            Log.i(TAG, "OnPictureTaken executed...");
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-            if (pictureFile == null){
-                Log.d(TAG, "Error creating media file, check storage permissions");
-                return;
-            }
-
-            viewModel.getExecutorService().execute(() -> {
-                try {
-                    FileOutputStream fos = new FileOutputStream(pictureFile);
-                    fos.write(data);
-                    fos.close();
-                    JSONObject requestMessage = JsonRequestUtils.generateJsonMessage(pictureFile);
-                    sendImageToServer(requestMessage);
-                } catch (FileNotFoundException e) {
-                    Log.d(TAG, "File not found: " + e.getMessage());
-                } catch (IOException e) {
-                    Log.d(TAG, "Error accessing file: " + e.getMessage());
-                }
-            });
-        }
-    };
-
-    private void releaseCamera() {
-        mCamera.stopPreview();
-        mCamera.setPreviewCallback(null);
-
-        mCamera.release();
-        mCamera = null;
-    }
-
     private void sendImageToServer(JSONObject requestJson) {
         // for testing
         viewModel.getExecutorService().execute(() -> {
